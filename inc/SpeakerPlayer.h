6317e554f0a53754b402d49e6991b1a0422e9e89
Stefan Ardeleanu <stefan.ardeleanu@ausy.com>
2021-09-20 09:13:35 +0300

    Added SpeakerPlayer.cpp

--- inc/SpeakerPlayer.h
+++ inc/SpeakerPlayer.h
@@ -21,54 +21,11 @@ private:
     std::vector<Mix_Chunk *> m_sounds;
 
 public:
-    SpeakerPlayer()
-    {
-    }
-
-    ~SpeakerPlayer()
-    {
-        SDL_Quit();
-    }
-
-    void init(const std::string &path)
-    {
-        JsonParser parser(path);
-
-        SDL_Init(SDL_INIT_AUDIO);
-
-        if (Mix_OpenAudio(AUDIO_RATE, AUDIO_FORMAT, AUDIO_CHANNELS, AUDIO_BUFFERS) != 0)
-        {
-            exit(-1);
-        }
-
-        for (int i = 0; i < NUMBER_OF_BUTTONS; i++)
-        {
-            Mix_Chunk *tmpChunck = Mix_LoadWAV(parser.getSoundPathByButtonID(i).c_str());
-
-            if (tmpChunck != nullptr)
-            {
-                m_sounds.push_back(tmpChunck);
-            }
-        }
-    }
-
-    Mix_Chunk *getSoundById(int index)
-    {
-        try
-        {
-            return m_sounds[index];
-        }
-        catch (...)
-        {
-            return nullptr;
-        }
-    }
-
-    virtual void playSound(int index)
-    {
-        if (getSoundById(index) != nullptr)
-            Mix_PlayChannel(1, getSoundById(index), 0);
-    }
+    SpeakerPlayer();
+    ~SpeakerPlayer();
+    void init(const std::string &path);
+    Mix_Chunk *getSoundById(int index);
+    virtual void playSound(int index);
 };
 
 #endif
---
+++ src/SpeakerPlayer.cpp
@@ -0,0 +1,50 @@
+#include "SpeakerPlayer.h"
+
+SpeakerPlayer::SpeakerPlayer()
+{
+}
+
+SpeakerPlayer::~SpeakerPlayer()
+{
+    SDL_Quit();
+}
+
+void SpeakerPlayer::init(const std::string &path)
+{
+    JsonParser parser(path);
+
+    SDL_Init(SDL_INIT_AUDIO);
+
+    if (Mix_OpenAudio(AUDIO_RATE, AUDIO_FORMAT, AUDIO_CHANNELS, AUDIO_BUFFERS) != 0)
+    {
+        exit(-1);
+    }
+
+    for (int i = 0; i < NUMBER_OF_BUTTONS; i++)
+    {
+        Mix_Chunk *tmpChunck = Mix_LoadWAV(parser.getSoundPathByButtonID(i).c_str());
+
+        if (tmpChunck != nullptr)
+        {
+            m_sounds.push_back(tmpChunck);
+        }
+    }
+}
+
+Mix_Chunk *SpeakerPlayer::getSoundById(int index)
+{
+    try
+    {
+        return m_sounds[index];
+    }
+    catch (...)
+    {
+        return nullptr;
+    }
+}
+
+void SpeakerPlayer::playSound(int index)
+{
+    if (getSoundById(index) != nullptr)
+        Mix_PlayChannel(1, getSoundById(index), 0);
+}
