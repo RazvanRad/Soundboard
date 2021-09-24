3eaf047f297812c0decc35d0f5a95e042dc12b7a
Iancu Vlad <vlad.iancu@ausy.com>
2021-09-24 10:55:41 +0300

    Added BluetoothListener doc

--- inc/BluetoothListener.hpp
+++ inc/BluetoothListener.hpp
@@ -6,14 +6,35 @@
 #include <thread>
 #include <vector>
 
+/**
+ * Listens on a bluetooth socket for data represented as an integer
+ */
 class BluetoothListener : public IListener
 {
 private:
     std::thread bthread;
     int _active;
 public:
+    /**
+     * Starts a bluetooth socket on a separate thread
+     * @param handler Whenever the socket receives data from the client this function will be called with the respective data
+     */
+
     void start(Handler handler);
+    /**
+     * Stops the bluetooth socket and terminates the listener thread
+     */
+
     void stop();
+
+    /**
+     * Initializes the bluetooth configuration.
+     * Will run the following commands: \n
+     * `sudo systemctl daemon-reload` \n
+     * `sudo systemctl bluetooth restart` \n
+     * `sudo hciconfig <device> piscan` \n
+     *  where `<device`> is the first bluetooth device found (returned by `hcitool dev`)
+     */
     int init();
     int isActive() const;
 };
